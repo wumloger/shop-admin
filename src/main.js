dfa7530028d2eb6d5ec40dcb62b4fd6586c4ecab
ray <656482519@qq.com>
2023-04-11 14:10:25 +0800

    全局引入图标

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import elementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import '../node_modules/element-plus/dist/index.css'
 import 'virtual:windi.css'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(elementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(router)
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Index from '~/pages/index.vue'
 import About from '~/pages/about.vue'
 import NotFount from '~/pages/404.vue'
+import Login from '~/pages/Login.vue'
 
 const routes = [
     {
@@ -11,6 +12,10 @@ const routes = [
     {
         path: '/about',
         component: About
+    },
+    {
+        path: '/login',
+        component: Login
     },
     {
         path: '/:pathMatch(.*)',
